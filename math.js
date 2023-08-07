88bd53d85a5040047477f15eee593b4c55a6cf0a
jayshree jaiswal <jayshree.jaiswal@codecraft.co.in>
2023-08-07 16:45:37 +0530

    implementing divide function

--- math.js
+++ math.js
@@ -1,1 +1,11 @@
 //this modulr is going to contain add ,mul,sub,div function
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns  result
+ */
+function divide(a,b){
+    return a/b;
+}
+
