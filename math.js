0f55d5c91145f4e59ff90c8ef9d52a9ef53b18e1
jayshree jaiswal <jayshree.jaiswal@codecraft.co.in>
2023-08-07 17:16:47 +0530

    implementing multiply function

--- math.js
+++ math.js
@@ -1,1 +1,10 @@
 //this modulr is going to contain add ,mul,sub,div function
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns result
+ */
+function multiply(a,b){
+    return a*b;
+}
