c9849e60321a9035283290928c58c076dc578b8b
jayshree jaiswal <jayshree.jaiswal@codecraft.co.in>
2023-08-07 17:32:11 +0530

    implementing subtract function

--- math.js
+++ math.js
@@ -1,1 +1,11 @@
 //this modulr is going to contain add ,mul,sub,div function
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns result
+ */
+ function subtract(a,b){
+    return a-b;
+ }
