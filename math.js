6559c203b3e68d5ab238062ba62f8596c04f1996
HIMANSHUKESARWA\himanshukesarwani <himanshu.k@codecraft.co.in>
2023-08-14 17:26:24 +0530

    feat: implemented the profile page function

--- math.js
+++ math.js
@@ -63,3 +63,10 @@ function arithmetic(a,b)
 function pythagorasTheoremFunction(base, height){
     return Math.sqrt((base*base)+(height*height));
 }
+
+
+// code by himanshu.
+
+function profilePage(img){
+    return "image"
+}
